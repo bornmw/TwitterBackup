a2f62ebba746c2c75f3db0d5e5b1e0e19815f636
omikheev <omikheev@omtux.(none)>
2012-03-13 17:33:57 +0400

    works, executables as SH script

--- twitter_backup.rb
+++ twitter_backup.rb
@@ -1,11 +1,24 @@
+#!/bin/sh
+RUBY_EXEC=`which ruby`
+if [ -n "$RUBY_EXEC" ]; then
+  "$RUBY_EXEC" -x $0 $1 $2 $3
+  exit $?
+fi
+
+#!/usr/bin/env ruby -w
+require 'csv'
 require 'json'
 require "net/http"
 
 USER_TIMELINE_URI = URI.parse("https://api.twitter.com/1/statuses/user_timeline.json")
 
-def get_timeline_part(count, max_id = nil)
+def usage
+  puts 'Usage: twitter_backup.sh screen_name [file_name]'
+end
+
+def get_timeline_part(screen_name, count, max_id = nil)
   args = {include_entities: 0, include_rts: 0,
-          screen_name: 'bornmw', count: count, trim_user: 1,
+          screen_name: screen_name, count: count, trim_user: 1,
           page: 2, max_id: max_id}
   USER_TIMELINE_URI.query = URI.encode_www_form(args)
   http = Net::HTTP.new(USER_TIMELINE_URI.host, USER_TIMELINE_URI.port)
@@ -14,14 +27,41 @@ def get_timeline_part(count, max_id = nil)
 
   request = Net::HTTP::Get.new(USER_TIMELINE_URI.request_uri)
 
-  response = http.request(request)
-
+  attempt = 0
   begin
-    json = JSON response.body
+    attempt+=1
+    response = http.request(request)
+    JSON response.body
   rescue
-    puts 'Failed to parse: ' + response.body
+    puts "Failed to parse (#{attempt}): " + response.body.scan(/title>(.+)<\/title/)
+    if attempt < 3 then
+      retry
+    else
+      raise
+    end
   end
-
 end
 
-puts get_timeline_part(2)
+if ARGV.length == 0
+  usage
+  exit 1
+end
+
+f = File.new 'TwitterBackup.csv', 'a'
+csv = CSV.new f, {headers: ['id_str', 'created_at', 'text'], write_headers: true}
+
+counter = 0
+last_id_str = nil
+while true
+  timeline_part = get_timeline_part(ARGV[0], 100, last_id_str)
+  if timeline_part.length == 0
+    puts 'Done!'
+    exit 0
+  end
+  timeline_part.each do |x|
+    csv << [x['id_str'], x['created_at'], x['text']]
+    last_id_str = x['id_str']
+    counter += 1
+  end
+  puts "processed #{counter}, last id was #{last_id_str}..."
+end
